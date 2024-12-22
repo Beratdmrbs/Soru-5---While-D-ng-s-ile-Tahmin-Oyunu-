e4a9417cd8aa9f2cb04d94699b1aa9911eec2580
Beratdmrbs <beratdmrbs61@gmail.com>
2024-12-22 17:05:56 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random rand = new Random();
+        int correctNumber = rand.Next(1, 101);
+        int userGuess = 0;
+
+        Console.WriteLine("Tahmin oyununa hoş geldiniz!");
+        Console.WriteLine("1 ile 100 arasında bir sayı tahmin edin.");
+
+        while (userGuess != correctNumber)
+        {
+            Console.Write("Tahmininizi girin: ");
+            userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess < correctNumber)
+            {
+                Console.WriteLine("Daha büyük bir sayı tahmin edin.");
+            }
+            else if (userGuess > correctNumber)
+            {
+                Console.WriteLine("Daha küçük bir sayı tahmin edin.");
+            }
+        }
+
+        Console.WriteLine("Tebrikler! Doğru tahmin ettiniz.");
+    }
+}
